7624f2ab286e0cc2b017eb229ca54160ba20d299
arlz1013 <jdavid.pforero@gmail.com>
2024-08-27 09:59:57 -0500

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: "/Presentation-For-IA/",
+  server: {
+    open: true, // Automatically open the presentation in the browser
+  },
 })
