dc548d39cc05eb415714968427bd707bee5def06
arlz1013 <jdavid.pforero@gmail.com>
2024-08-27 09:02:29 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Reveal from "reveal.js";
+import Markdown from "reveal.js/plugin/markdown/markdown.esm"
+
+let deck = new Reveal(
+  {
+    plugins : [Markdown]
+  }
+)
+
+deck.initialize();
